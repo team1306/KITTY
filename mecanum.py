c2c574d1072e407285318b877654048b78aabecd
James Tautges <jamesdtautges@gmail.com>
2013-07-23 18:37:46 -0500

    Added Mecanum class that calculates wheel speeds and generates bytes to send to Arduino

---
+++ mecanum.py
@@ -0,0 +1,38 @@
+from math import sin, cos
+
+class Mecanum:
+    def __init__(self, a, b, velocityRange=100**3, omegaRange=10**3): 
+        # the ranges are centered at 0, so the actual range of acceptable velocity values is [-velocityRange, velocityRange]
+        # the range is also the range for v_x and v_y, not |v|
+        self.a = a
+        self.b = b
+        self.maxVelocity = velocityRange
+        self.minVelocity = -velocityRange
+        self.maxOmega = omegaRange
+        self.minOmega = -omegaRange
+
+    # wheel numbers are assigned as follows from a top view
+    # 1: front right
+    # 2: front left
+    # 3: back left
+    # 4: back right
+
+    def getVelocities(self, vx, vy, omega):
+        vw1 = vy - vx + omega*(self.a + self.b)
+        vw2 = vy + vx - omega*(self.a + self.b)
+        vw3 = vy - vx - omega*(self.a + self.b)
+        vw4 = vy + vx + omega*(self.a + self.b)
+        return (vw1, vw2, vw3, vw4)
+
+    def scaleVelocities(self, velocities):
+        scaled = []
+        for v in velocities:
+            scaled.append(float(v*255/(2*(self.maxVelocity + self.maxVelocity + self.maxOmega*(self.a + self.b)))) + 127.5)
+        return scaled
+
+    def getBytes(self, vx, vy, omega):
+        velocities = self.scaleVelocities(self.getVelocities(vx, vy, omega))
+        b = []
+        for v in velocities:
+            b.append(chr(int(round(v))))
+        return b
