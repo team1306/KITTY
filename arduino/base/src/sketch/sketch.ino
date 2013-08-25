503670fa954f2365eac8aa28cc1e11b8727d38e8
James Tautges <jamesdtautges@gmail.com>
2013-08-24 23:02:30 -0500

    Proof of concept for new modular Arduino code

---
+++ arduino/base/src/sketch/sketch.ino
@@ -0,0 +1,26 @@
+bool onoff = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(Serial.available() > 3) {
+    int a = int(Serial.read());
+    int b = int(Serial.read());
+    int c = int(Serial.read());
+    int d = int(Serial.read());
+    if(a > 0) {
+      onoff = 0;
+    }
+    else {
+      onoff = 1;
+    }
+  }
+  if(onoff) {
+    digitalWrite(13, HIGH);
+  }
+  else {
+    digitalWrite(13, LOW);
+  }
+}
