8a51d289c2f44b5ed22d18ecc07be16aca4ec732
James Tautges <jamesdtautges@gmail.com>
2013-08-26 13:29:44 -0500

    No breaks for you

--- cgi-bin/data_wsh.py
+++ cgi-bin/data_wsh.py
@@ -12,8 +12,6 @@ def web_socket_transfer_data(request):
     s.connect(('127.0.0.1', 50007))
     while True:
         line = request.ws_stream.receive_message()
-        if line is None:
-            return
         request.ws_stream.send_message(line)
 
         s.send(line)
